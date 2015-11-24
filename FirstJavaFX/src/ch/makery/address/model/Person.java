e53544aa920059b91e62cd8cd0acc70e12a6e16e
afetrist <wojciech.zagrajczuk@gmail.com>
2015-11-24 18:54:37 +0100

    First pull

---
+++ FirstJavaFX/src/ch/makery/address/model/Person.java
@@ -0,0 +1,133 @@
+package ch.makery.address.model;
+
+import java.time.LocalDate;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+
+public class Person {
+
+    private final StringProperty firstName;
+    private final StringProperty lastName;
+    private final StringProperty position;
+    private final IntegerProperty salary;
+    private final StringProperty phone;
+    private final StringProperty date;
+    private final StringProperty email;
+
+    //private final ObjectProperty<LocalDate> birthday;
+
+    /**
+     * Default constructor.
+     */
+    public Person() {
+        this(null, null);
+    }
+
+    /**
+     * Constructor with some initial data.
+     *
+     * @param firstName
+     * @param lastName
+     */
+    public Person(String firstName, String lastName) {
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+
+        // Some initial dummy data, just for convenient testing.
+        this.position = new SimpleStringProperty("Witcher");
+        this.salary = new SimpleIntegerProperty(2000);
+        this.date = new SimpleStringProperty("15.07.1410");
+        this.email = new SimpleStringProperty("white@withers.com");
+        this.phone = new SimpleStringProperty("+48 123 456 789");
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public StringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public StringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public String getPosition() {
+        return position.get();
+    }
+
+    public StringProperty positionProperty() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position.set(position);
+    }
+
+    public int getSalary() {
+        return salary.get();
+    }
+
+    public IntegerProperty salaryProperty() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary.set(salary);
+    }
+
+    public String getPhone() {
+        return phone.get();
+    }
+
+    public StringProperty phoneProperty() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone.set(phone);
+    }
+
+    public String getDate() {
+        return date.get();
+    }
+
+    public StringProperty dateProperty() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date.set(date);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public StringProperty emailProperty() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+}
